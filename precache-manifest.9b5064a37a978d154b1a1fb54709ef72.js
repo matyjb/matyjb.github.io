2b6e3ad783e93488c605c2f0ec5a2d446f237836
Matyjasiak Bartosz <matyjasiakbartosz@gmail.com>
2020-06-15 17:40:10 +0200

    pwi latest

---
+++ precache-manifest.9b5064a37a978d154b1a1fb54709ef72.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2dad00a044d4f132d88ba7cedf939fbd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "175e89a1ad4410e7787a",
+    "url": "/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "9cbafbacbab35020e7b3",
+    "url": "/static/css/main.19c5fde0.chunk.css"
+  },
+  {
+    "revision": "175e89a1ad4410e7787a",
+    "url": "/static/js/2.2dd3a0eb.chunk.js"
+  },
+  {
+    "revision": "9cbafbacbab35020e7b3",
+    "url": "/static/js/main.27786282.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "fa60d7235a9a7b94748bbcdead6d09bf",
+    "url": "/static/media/GK.fa60d723.png"
+  },
+  {
+    "revision": "24310287da1d396ed4c44dc18a7df4d1",
+    "url": "/static/media/opsu.24310287.png"
+  },
+  {
+    "revision": "51a4b8fbf69d565581ba8ad4b82d05e2",
+    "url": "/static/media/react.51a4b8fb.png"
+  }
+]);
